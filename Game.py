e622c138f74023bbb05495563ff40e8ce9b50012
Shuja-ur-Rehman-Dar <y6g76@students.keele.ac.uk>
2026-03-15 12:41:19 +0000

    3rd Commit: Add basic logic to play the game

--- Game.py
+++ Game.py
@@ -6,3 +6,18 @@ user_score = 0
 computer_score = 0
 
 print("Welcome to Rock Paper Scissors")
+
+while True:
+
+    user = input("Enter rock, paper, scissors or quit: ").lower()
+
+    if user == "quit":
+        break
+
+    if user not in choices:
+        print("Invalid choice")
+        continue
+
+    computer = random.choice(choices)
+
+    print("Computer chose:", computer)
