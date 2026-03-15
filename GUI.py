f9497a9d70d25046fde577713c70ac2a7190b9fc
Shuja-ur-Rehman-Dar <y6g76@students.keele.ac.uk>
2026-03-15 13:02:17 +0000

    6th Commit: Adding the base code for players of the game in GUI file

--- GUI.py
+++ GUI.py
@@ -3,3 +3,17 @@ import random
 from tkinter import messagebox
 
 choices = ["rock", "paper", "scissors"]
+
+def play(user):
+    computer = random.choice(choices)
+
+    if user == computer:
+        result = "Tie!"
+    elif (user == "rock" and computer == "scissors") or \
+         (user == "paper" and computer == "rock") or \
+         (user == "scissors" and computer == "paper"):
+        result = "You win!"
+    else:
+        result = "You lose!"
+
+    messagebox.showinfo("Result", f"Computer chose {computer}\n{result}")
